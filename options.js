ed6836b6dbaec9e2b13648585c6f0f488f8a48b8
Mikhail Aksenov <Mikhail.Aksenov@dataart.com>
2015-11-20 21:41:09 +0300

    Add options for replacements. (Just elements, without save/load/work)

--- options.js
+++ options.js
@@ -56,13 +56,47 @@ function save_options() {
 
 // Restores select box state to saved value from localStorage.
 function restore_options() {
-    var opts = JSON.parse(localStorage["quick_url_copy"]);
+    var jsonOpts = localStorage["quick_url_copy"];
+    if(!jsonOpts) {
+        return;
+    }
+    var opts = JSON.parse(jsonOpts);
     for (var i = 0; i < options.length; i++) {
         var id = options[i];
             // console.log("i:" + i + " = " + id )
         load_option(opts, id)
     }
-
 }
+
+function add_new_rule(){
+    var defRule = document.getElementById("defaultRule");
+    var c = defRule.children[0].cloneNode(true);
+    var rulesNodes = document.querySelector('#rules');
+    rulesNodes.appendChild(c.cloneNode(true));
+}
+
+function load_rules()
+{
+    var rules = [];
+    var loadRule = function(el){
+        var elems = el.querySelectorAll('input');
+        var dict = {};
+
+        [].forEach.call(elems, function(obj) {
+            dict[obj.name]= obj.value;
+        });
+
+        if (Object.keys(dict).length !== 0 && dict.host.length > 0)
+        {
+            rules.push(dict);
+        }
+    };
+
+    var rulesElements = document.querySelector('#rules').children;
+
+    [].forEach.call(rulesElements, loadRule);
+}
+
 document.addEventListener('DOMContentLoaded', restore_options);
 document.querySelector('#save').addEventListener('click', save_options);
+document.querySelector('#addRule').addEventListener('click', add_new_rule);

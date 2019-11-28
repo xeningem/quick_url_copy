240834d9fc22af8f834818d61e99a6e41b72c7e1
Mikhail Aksenov <xeningem@gmail.com>
2019-11-28 23:49:41 +0300

    Use navigator.clipboard.writeText instead of hacks

--- content.js
+++ content.js
@@ -46,15 +46,72 @@ function restore_options(local_storage) {
 
 }
 
+function getJiraTaskInfo(document_root) {
+    var issueKeyVal = document_root.getElementById("issuekey-val")
+    if (!issueKeyVal){
+        return "";
+    }
+
+    var summaryVal = document_root.getElementById("summary-val")
+    if (!summaryVal){
+        return "";
+    }
+
+    var issue = issueKeyVal.children[0]
+
+
+    return issue.textContent + " " + summaryVal.textContent + "\n" + issue.href;
+}
+
+
+function StringToSend(document_root, rules) {
+    // TODO: Возмоность выбора порядка полей
+    var url = window.location.href;
+    var host = window.location.hostname
+    var box = document_root.getElementById("copy-box");
+    var title = ( box ? box.value : window.document.title )
+
+    if(document_root.getElementById("jira") != null){
+        var issue_info = getJiraTaskInfo(document_root)
+        if(issue_info){
+            return issue_info
+        }
+    } 
+
+
+    if (rules && rules[host]){
+        rule = rules[host]
+        var re_url = new RegExp(rule.urlSearch)
+        if (url.match(re_url)){
+            url = url.replace(re_url, rule.urlReplace)
+        }
+
+        var re_title = new RegExp(rule.titleSearch)
+        if (title.match(re_title)){
+            title = title.replace(re_title, rule.titleReplace)
+        }
+    }
+
+    var textMsg = title + "\n " + url;
+
+    return textMsg;
+}
+
+
 document.addEventListener('keydown', function(event) {
     // Ctrl+Alt+C
     if (isCopyKey(event)) {
         // Dispatch a custom message, handled by your extension
         // console.log("Keydown:" + event + " " + event.which);
 
-        chrome.extension.sendMessage({
-            method: "backgroundCopy"
-        });
+
+        var textToSend = StringToSend(document, null)
+
+        navigator.clipboard.writeText(textToSend)
+          .catch(err => {
+            // This can happen if the user denies clipboard permissions:
+            console.error('Could not copy text: ', err);
+          });
     }
 },
 true); // <-- True is important
@@ -64,7 +121,6 @@ chrome.runtime.sendMessage({ method: "requestLocalStorage" });
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     // console.log(request);
     if (request.method == "responseLocalStorage" ) {
-        console.log("on content.js")
         var l_s = request.local_storage;
         if (  l_s["saved"] )
         {
@@ -75,11 +131,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 });
 
  chrome.storage.sync.get(['rules'], function(values) {
-          // Notify that we saved.
-     console.log('rules in content.js', values)
     if(!values["rules"]){
          chrome.storage.sync.set({'rules': rules}, function(values) {
-            console.log('set in content.js', values)
         });
     }
 
